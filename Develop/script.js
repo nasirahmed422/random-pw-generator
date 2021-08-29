14c2bc0f6506b148dc1f349da16626941defc615
Nasir Ahmed <nahmed@utilant.com>
2021-08-29 16:24:19 -0400

    adding helper functions for letters, number, and characters

--- Develop/script.js
+++ Develop/script.js
@@ -15,6 +15,32 @@ var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var character = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', ',', '.', '?', ';', ':', '{', '}', '[', ']', '|'];
 
+//These will help store the password in our main generatePassword function. 
+var passwordChar = '';
+var newPassword;
+
+//This is the function to return a random lowercase letter.
+function getLowerCase() {
+  var selectRandom = Math.floor(Math.random() * 26);
+  return alpha[selectRandom];
+}
+//This is the function to return a random uppercase letter.
+function getUpperCase() {
+  var selectRandom = Math.floor(Math.random() * 26);
+  //This is the same as getLowerCase, but with the toUpperCase() added.
+  return alpha[selectRandom].toUpperCase();
+}
+//This is the function to return a random number.
+function getNumber() {
+  var selectRandom = Math.floor(Math.random() * 10);
+  return number[selectRandom];
+}
+//This is the function to return a random special character.
+function getSpecialCharacter() {
+  var selectRandom = Math.floor(Math.random() * 26);
+  return character[selectRandom];
+}
+
 
 // Write password to the #password input
 function writePassword() {
